f6565c1c553c3a9377b292077ae9bcff53714a74
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-12-04 19:16:34 -0800

    restart project

--- nes/src/mapper.rs
+++
@@ -1,8 +0,0 @@
-pub trait Mapper {
-    fn cpu_map_read(&mut self, addr: u16, mapped_addr: &mut u32) -> bool;
-    fn cpu_map_write(&mut self, addr: u16, mapped_addr: &mut u32) -> bool;
-    fn ppu_map_read(&mut self, addr: u16, mapped_addr: &mut u32) -> bool;
-    fn ppu_map_write(&mut self, addr: u16, mapped_addr: &mut u32) -> bool;
-
-    fn new(prg_banks: u8, chr_banks: u8) -> Self;
-}
